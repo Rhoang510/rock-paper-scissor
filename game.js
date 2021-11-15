7c8e85df9d144549187152c462827e4f533ba559
Roger <rhoang510@gmail.com>
2021-11-15 13:42:56 -0800

    Add index.html

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
   
   let userScore = 0;
   let computerScore = 0;
-  
+  let humanScore = document.querySelector("#humanScore");
+  let compScore = document.querySelector("#compScore");
 
   document.getElementById('rock').onclick = user;
   document.getElementById('paper').onclick = user;
@@ -10,12 +11,11 @@
   function user(){
     let userChoice = this.id;
     const computerChoice = computerPlay(); 
-    if (userChoice == 5 || computerChoice == 5) {
-        return winCondition;
+    if (userScore === 5 || computerScore === 5) {
+        return winCondition();
+    } else {
+      playRound(userChoice, computerChoice);
     }
-    console.log(playRound(userChoice, computerChoice));
-    console.log("Your score = " + userScore);
-    console.log("Computer's score = " + computerScore);
   }
 
   function computerPlay() {
@@ -32,30 +32,36 @@
     // Rock Choice
     if (userChoice == "rock" && computerChoice == "scissors") {
       userScore++;
-      return "You win! Rock beats scissors!";
+      humanScore.innerHTML = userScore;
+      return document.getElementById("results").innerHTML = "You win! Rock beats scissors!";
     } else if (userChoice == "rock" && computerChoice == "paper") {
       computerScore++;
-      return "You lose! Scissors beats rock";
+      compScore.innerHTML = computerScore;
+      return document.getElementById("results").innerHTML = "You lose! Scissors beats rock";
     }
     // Paper Choice
     else if (userChoice == "paper" && computerChoice == "rock") {
       userScore++;
-      return "You win! Paper beats rock";
+      humanScore.innerHTML = userScore;
+      return document.getElementById("results").innerHTML = "You win! Paper beats rock";
     } else if (userChoice == "paper" && computerChoice == "scissors") {
       computerScore++;
-      return "You lose! Scissors beat paper!";
+      compScore.innerHTML = computerScore;
+      return document.getElementById("results").innerHTML = "You lose! Scissors beat paper!";
     }
     // Scissors Choice
     else if (userChoice == "scissors" && computerChoice == "rock") {
       computerScore++;
-      return "You lose! Rock beats scissors";
+      compScore.innerHTML = computerScore;
+      return document.getElementById("results").innerHTML = "You lose! Rock beats scissors";
     } else if (userChoice == "scissors" && computerChoice == "paper") {
       userScore++;
-      return "You win! Scissor beats rock!";
+      humanScore.innerHTML = userScore;
+      return document.getElementById("results").innerHTML = "You win! Scissor beats rock!";
     } 
       // Tie
     else if (userChoice === computerChoice) {
-      return "It's a draw!";
+      return document.getElementById("results").innerHTML = "It's a draw!";
     } else {
       return "Error, please choose rock, paper, or scissors";
     }
@@ -63,11 +69,9 @@
      // End game alert
   function winCondition() {
     if (userScore > computerScore) {
-    return "Congrats! You win!";
+    return document.getElementById("results").innerHTML = "Congrats! You win the game!";
     } else if (userScore < computerScore) {
-      return "Sorry, you lose.";
-    } else {
-      return "It's a draw!";
+      return document.getElementById("results").innerHTML = "Sorry, you lost the game.";
     }
 
   }
