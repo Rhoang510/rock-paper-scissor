ca0095dc99609052dd75a86a22a6ba8728a99517
Roger <rhoang510@gmail.com>
2021-11-16 13:01:48 -0800

    Add game.js style.css

--- game.js
+++ game.js
@@ -1,22 +1,34 @@
   
   let userScore = 0;
   let computerScore = 0;
-  let humanScore = document.querySelector("#humanScore");
-  let compScore = document.querySelector("#compScore");
+  const buttons = document.querySelectorAll("button");
+  const humanScore = document.querySelector("#humanScore");
+  const compScore = document.querySelector("#compScore");
+  const reset = document.querySelector("#reset");
+  const results = document.getElementById("results");
 
-  document.getElementById('rock').onclick = user;
-  document.getElementById('paper').onclick = user;
-  document.getElementById('scissors').onclick = user;
-  
-  function user(){
-    let userChoice = this.id;
-    const computerChoice = computerPlay(); 
-    if (userScore === 5 || computerScore === 5) {
-        return winCondition();
-    } else {
-      playRound(userChoice, computerChoice);
-    }
-  }
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const userChoice = button.id;
+      const computerChoice = computerPlay();
+      if (userScore === 5 || computerScore === 5) {
+        return winCondition(); 
+      } else {
+        playRound(userChoice, computerChoice);
+      }
+    });
+  });
+
+  reset.addEventListener("click", () => {
+      userScore = 0;
+      computerScore = 0;
+      compScore.textContent = 0;
+      humanScore.textContent = 0;
+      results.removeAttribute("style");
+      results.textContent = "You have restarted the game.";
+  });
+
+ 
 
   function computerPlay() {
     let computerPlay = Math.random();
@@ -28,50 +40,51 @@
       return "scissors";
     }
   }
-  function playRound( userChoice, computerChoice ) {
+  function playRound(userChoice, computerChoice) {
     // Rock Choice
     if (userChoice == "rock" && computerChoice == "scissors") {
       userScore++;
-      humanScore.innerHTML = userScore;
-      return document.getElementById("results").innerHTML = "You win! Rock beats scissors!";
+      humanScore.textContent = userScore;
+      return results.textContent = "You win! Rock beats scissors!";
     } else if (userChoice == "rock" && computerChoice == "paper") {
       computerScore++;
-      compScore.innerHTML = computerScore;
-      return document.getElementById("results").innerHTML = "You lose! Scissors beats rock";
+      compScore.textContent = computerScore;
+      return results.textContent = "You lose! Scissors beats rock";
     }
     // Paper Choice
     else if (userChoice == "paper" && computerChoice == "rock") {
       userScore++;
-      humanScore.innerHTML = userScore;
-      return document.getElementById("results").innerHTML = "You win! Paper beats rock";
+      humanScore.textContent = userScore;
+      return results.textContent = "You win! Paper beats rock";
     } else if (userChoice == "paper" && computerChoice == "scissors") {
       computerScore++;
-      compScore.innerHTML = computerScore;
-      return document.getElementById("results").innerHTML = "You lose! Scissors beat paper!";
+      compScore.textContent = computerScore;
+      return results.textContent = "You lose! Scissors beat paper!";
     }
     // Scissors Choice
     else if (userChoice == "scissors" && computerChoice == "rock") {
       computerScore++;
-      compScore.innerHTML = computerScore;
-      return document.getElementById("results").innerHTML = "You lose! Rock beats scissors";
+      compScore.textContent = computerScore;
+      return results.textContent = "You lose! Rock beats scissors";
     } else if (userChoice == "scissors" && computerChoice == "paper") {
       userScore++;
-      humanScore.innerHTML = userScore;
-      return document.getElementById("results").innerHTML = "You win! Scissor beats rock!";
+      humanScore.textContent = userScore;
+      return results.textContent = "You win! Scissor beats rock!";
     } 
       // Tie
     else if (userChoice === computerChoice) {
-      return document.getElementById("results").innerHTML = "It's a draw!";
-    } else {
-      return "Error, please choose rock, paper, or scissors";
+      return results.textContent = "It's a draw!";
     }
   }
      // End game alert
   function winCondition() {
+    results.style.color = "red";
+    results.style.fontSize = "40px";
+    results.style.fontWeight = "bold";
     if (userScore > computerScore) {
-    return document.getElementById("results").innerHTML = "Congrats! You win the game!";
+    return results.textContent = "Congrats! You win the game!!!";
     } else if (userScore < computerScore) {
-      return document.getElementById("results").innerHTML = "Sorry, you lost the game.";
+      return results.textContent = "Sorry, you lost the game.";
     }
 
   }
